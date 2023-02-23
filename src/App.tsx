e613f26988288cd06bf0bcbebc0071353397a9fa
pengshixing <2644378911@qq.com>
2023-02-23 23:08:06 +0800

    feat: 添加错误边界

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css";
 import { useAuth } from "context/auth-context";
 import { AuthenticatedApp } from "screens/authenticated-app";
 import { UnauthenticatedApp } from "screens/unauthenticated-app";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback } from "components/FullPageErrorFallback";
 
 function App() {
   const { user } = useAuth();
 
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
